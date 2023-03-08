1dd34a3c5b166d418e9b075001c1585db93f3411
StoryOff <sotic.roma@gmail.com>
2023-03-08 09:02:11 +0800

    update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: ru, en etc.")
+                     help="Choose language: ru, en, es etc.")
 
 
 @pytest.fixture(scope="function")
