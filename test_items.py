fdac0cffdf922b166fe4ef90d62017027398dd6f
StoryOff <sotic.roma@gmail.com>
2023-03-08 21:31:25 +0800

    added assert

--- test_items.py
+++ test_items.py
@@ -7,4 +7,8 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_check_add_to_cart_button_exist(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    # задержка во избежание лагов сервера
+    time.sleep(2)
+    add_to_cart_button = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
+    assert(len(add_to_cart_button) > 0), 'Кнопка добавления в корзину не найдена'
     time.sleep(30)
