d789ea2a2c088734903364d7beba649e7bb936ea
StoryOff <sotic.roma@gmail.com>
2023-03-08 21:07:35 +0800

    update time.sleep

--- test_items.py
+++ test_items.py
@@ -7,4 +7,4 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_check_add_to_cart_button_exist(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
-    time.sleep(10)
+    time.sleep(30)
